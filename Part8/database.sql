182bf997278160a266de09fb4bda27868e2b0923
Dantony26 <dufourantony2612@gmail.com>
2021-07-13 17:08:21 +0200

    commit

---
+++ Part8/database.sql
@@ -0,0 +1,23 @@
+Exercice 1 :
+
+SELECT `frameworks`.`name`, `languages`.`name`
+FROM `frameworks`
+INNER JOIN `languages` ON `frameworks`.`languagesId` = `languages`.`id`;
+
+Exercice 2 :
+
+SELECT `frameworks`.`name`, `languages`.`name`
+FROM `frameworks`
+LEFT JOIN `languages` ON `frameworks`.`languagesId` = `languages`.`id`;
+
+Exercice 3 :
+
+SELECT
+	`languages`.`id`,
+	`languages`.`name`,
+	COUNT(*)
+FROM
+	`frameworks`
+LEFT JOIN `languages` ON `frameworks`.`languagesId` = `languages`.`id`
+GROUP BY
+	`languages`.`id`;
